e7a0a2d45afd8f1b3656ecae00fac58ea6882c9b
alimarchi <marchi_alice@yahoo.com>
2023-01-23 20:25:43 +0100

    userModel and usercontroller ready

--- Controller/userController.js
+++ Controller/userController.js
@@ -2,29 +2,115 @@
 const express = require("express");
 const router = express.Router();
 
+// Importamos el modelo con el schema correspondiente
+const Model = require("../Model/userModel");
+
 // Escuchar peticiones get
-router.get("/", (req, res) => {
-  res.send("GET collection");
+router.get("/", async (req, res) => {
+  // Model.find()
+  //   .then((data) =>
+  //     res.status(200).json({ status: "succeeded", data, error: null })
+  //   )
+  //   .catch((error) =>
+  //     res.status(404).json({
+  //       status: "failed",
+  //       data: null,
+  //       // ponemos null a data porque está fallando y no hay nada que enviar
+  //       error: error.message,
+  //     })
+  //   );
+  try {
+    const data = await Model.find();
+    res.status(200).json({ status: "succeeded", data, error: null });
+  } catch (error) {
+    res.status(404).json({
+      status: "failed",
+      data: null,
+      // ponemos null a data porque está fallando y no hay nada que enviar
+      error: error.message,
+    });
+  }
 });
 
 // Obtener documentos por id
 router.get("/:id", (req, res) => {
-  res.send(`GET document id ${req.params.id}`);
+  Model.findById(req.params.id)
+    .exec()
+    .then((data) =>
+      res.status(200).json({ status: "succeeded", data, error: null })
+    )
+    .catch((error) =>
+      res.status(404).json({
+        status: "failed",
+        data: null,
+        // ponemos null a data porque está fallando y no hay nada que enviar
+        error: error.message,
+      })
+    );
 });
 
 // Recibir documentos POST
 router.post("/", (req, res) => {
-  res.send("POST collection");
+  const data = new Model({
+    name: req.body.name,
+    lastname: req.body.lastname,
+    email: req.body.email,
+    role: req.body.role,
+    skills: req.body.skills,
+    personality: req.body.personality,
+  });
+  // estoy recogiendo la información de request que me llega del navegador
+  data
+    .save()
+    .then((data) =>
+      res.status(201).json({ status: "succeeded", data, error: null })
+    )
+    .catch((error) =>
+      res.status(404).json({
+        status: "failed",
+        data: null,
+        // ponemos null a data porque está fallando y no hay nada que enviar
+        error: error.message,
+      })
+    );
 });
 
 // Actualizar documentos por id PATCH
 router.patch("/:id", (req, res) => {
-  res.send(`UPDATE document id ${req.params.id}`);
+  let id = req.params.id;
+  let data = req.body;
+  const options = {
+    new: true,
+  };
+  Model.findByIdAndUpdate(id, data, options)
+    .then((data) =>
+      res.status(200).json({ status: "succeeded", data, error: null })
+    )
+    .catch((error) =>
+      res.status(404).json({
+        status: "failed",
+        data: null,
+        // ponemos null a data porque está fallando y no hay nada que enviar
+        error: error.message,
+      })
+    );
 });
 
 // Borrar documentos DELETE
 router.delete("/:id", (req, res) => {
-  res.send(`DELETE document id ${req.params.id}`);
+  let id = req.params.id;
+  Model.findByIdAndDelete(id)
+    .then((data) =>
+      res.status(200).json({ status: "succeeded", data, error: null })
+    )
+    .catch((error) =>
+      res.status(404).json({
+        status: "failed",
+        data: null,
+        // ponemos null a data porque está fallando y no hay nada que enviar
+        error: error.message,
+      })
+    );
 });
 
 module.exports = router;
